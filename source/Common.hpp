7440dd5870665bcc505300fe9e6ab5ff71054616
Dimo Markov <dimomarkov@gmail.com>
2024-03-15 12:35:16 +0200

    Code cleanup; Fixing missing external

--- source/Common.hpp
+++ source/Common.hpp
@@ -31,53 +31,6 @@ namespace Langulus
 
 } // namespace Langulus::Entity
 
-namespace Langulus::A
-{
-
-   /*class Module;
-   struct Unit;
-
-   struct PlatformModule;
-   struct Platform;
-   struct Window;
-   struct Cursor;
-
-   struct PhysicalModule;
-   struct Physical;
-   struct World;
-   struct Instance;
-
-   struct FileSystem;
-   struct File;
-   struct Folder;
-
-   struct GraphicsModule;
-   struct Graphics;
-   struct Renderer;
-   struct Layer;
-   struct Camera;
-   struct Renderable;
-   struct Light;
-
-   struct AssetModule;
-   struct Asset;
-   struct Mesh;
-   struct Material;
-   struct Image;
-
-   namespace UI
-   {
-
-      struct Module;
-      struct Unit;
-      struct System;
-      struct Button;
-      struct Text;
-      struct Input;
-
-   }*/ // namespace Langulus::A::UI
-
-} // namespace Langulus::A
 
 /// Make the rest of the code aware, that Langulus::Entity has been included  
 #define LANGULUS_LIBRARY_ENTITY() 1
