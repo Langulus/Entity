bd6ef8bb9a1e3eca45d5c2ad9b1f76be2ecdf5ad
Dimo Markov <dimomarkov@gmail.com>
2024-07-10 16:43:39 +0300

    Minor improvements

--- source/Thing.cpp
+++ source/Thing.cpp
@@ -11,7 +11,7 @@
 #if 0
    #define ENTITY_VERBOSE_ENABLED()       1
    #define ENTITY_VERBOSE_SELF(...)       Logger::Verbose(this, ": ", __VA_ARGS__)
-   #define ENTITY_VERBOSE_SELF_TAB(...)   const auto scoped = Logger::Verbose(this, ": ", __VA_ARGS__, Logger::Tabs {})
+   #define ENTITY_VERBOSE_SELF_TAB(...)   const auto scoped = Logger::VerboseTab(this, ": ", __VA_ARGS__)
    #define ENTITY_VERBOSE(...)            Logger::Append(__VA_ARGS__)
 #else
    #define ENTITY_VERBOSE_ENABLED()       0
@@ -215,7 +215,7 @@ namespace Langulus::Entity
 
    /// Dump the entity's hierarchy in log                                     
    void Thing::DumpHierarchy() const {
-      const auto tab = Logger::Verbose("** ", *this, Logger::Tabs {});
+      const auto tab = Logger::VerboseTab("** ", *this);
 
       if (mTraits) {
          Logger::Verbose(".. contains ", mTraits.GetCount(), " traits:");
